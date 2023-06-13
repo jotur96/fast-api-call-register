ce7520e16120ef34ec33ac704e2d182f83d11a89
Julio Olmedo <turlan125@gmail.com>
2023-06-13 16:19:32 -0400

    fix nose que me poaso

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # Python
+from fastapi import FastAPI
 from typing import Optional
 from enum import Enum
 
 time_limit = "1690862400000"
 
 
+app = FastAPI()
+
 @app.get('/')
 def home():
     return {"message": "Hello, world!"}
 
-@add.get('/validate')
+@app.get('/validate')
 def validate():
     return time_limit

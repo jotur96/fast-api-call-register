aae0219e976f52c0bd61814f91c79d49601a339b
Julio Olmedo <turlan125@gmail.com>
2023-06-13 16:29:46 -0400

    add main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 from typing import Optional
 from enum import Enum
+import uvicorn
 
 time_limit = "1690862400000"
 
@@ -15,3 +16,8 @@ def home():
 @app.get('/validate')
 def validate():
     return time_limit
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
